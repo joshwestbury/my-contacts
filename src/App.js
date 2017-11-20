e0d06c87d30b425f683c0f5e4ff29cd5ac2e208c
joshwestbury <joshwestbury@gmail.com>
2017-11-20 14:00:53 -0600

    added contactform component and address text fields

--- src/App.js
+++ src/App.js
@@ -1,18 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import AppBar from 'material-ui/AppBar';
+
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import ContactForm from './contact-form';
+
+//customize your theme
+//The 'theme' variable gets passed into the MuiThemeProvider component below.
+const theme = getMuiTheme({
+    palette: {primary1Color: '#E91E63',
+                   accent1Color: '#009688',
+    }
+})
 
 class App extends Component {
   render() {
+    //MuiThemeProvider is a component that takes a theme as a property and passes it down with context. This should preferably be at the root of your component tree.
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <MuiThemeProvider muiTheme={theme}>
+            <AppBar title="MyContacts"/>
+            <ContactForm/>
+        </MuiThemeProvider>
       </div>
     );
   }
